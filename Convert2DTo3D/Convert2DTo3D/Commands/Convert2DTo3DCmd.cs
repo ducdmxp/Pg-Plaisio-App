d69dc28ea3277f7aa6cf313b2ef2cf23cb553bc5
DinhMinhDuc <d.m.duc@ttdesignco.com>
2026-06-11 20:00:13 +0700

    Update Convert2DTo3DCmd.cs

--- Convert2DTo3D/Convert2DTo3D/Commands/Convert2DTo3DCmd.cs
+++ Convert2DTo3D/Convert2DTo3D/Commands/Convert2DTo3DCmd.cs
@@ -8,11 +8,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ParameterUtils = Convert2DTo3D.Utils.ParameterUtils;
 
 namespace Convert2DTo3D.Commands
 {
     [Transaction(TransactionMode.Manual)]
-    internal class Convert2DTo3DCmd : IExternalCommand
+    public class Convert2DTo3DCmd : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -22,35 +23,58 @@ namespace Convert2DTo3D.Commands
 
             var symbolId = doc.GetDefaultFamilyTypeId(new ElementId(BuiltInCategory.OST_Walls));
 
-            List<Line> lines = SelectLines(uiDoc);
+            List<ModelLine> lines = SelectLines(uiDoc);
             if (lines.Count == 0)
             {
                 TaskDialog.Show("Convert 2D to 3D", "No lines selected.");
                 return Result.Cancelled;
             }
 
+            List<ModelLine> tuongchinhs = GetLinesByStyle(lines, "NOSIVO");
+
+            uiDoc.Selection.SetElementIds(tuongchinhs.Select(line => line.Id).ToList());
+
             return Result.Succeeded;
         }
 
-        public static List<Line> SelectLines(UIDocument uidoc)
+        public List<ModelLine> GetLinesByStyle(List<ModelLine> lines, string styleName)
         {
-            List<Line> curves = new List<Line>();
+            return lines.Where(line => GetLineStyleName(line) == styleName).ToList();
+        }
+
+        public string GetLineStyleName(ModelLine line)
+        {
+            if (ParameterUtils.GetValueParameterByBuilt(line, BuiltInParameter.BUILDING_CURVE_GSTYLE) is ElementId lineStyleId)
+            {
+                if (lineStyleId != ElementId.InvalidElementId)
+                {
+                    var lineStyle = line.Document.GetElement(lineStyleId);
+                    if (lineStyle != null)
+                    {
+                        return lineStyle.Name;
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public static List<ModelLine> SelectLines(UIDocument uidoc)
+        {
+            List<ModelLine> curves = new List<ModelLine>();
 
             try
             {
                 curves = uidoc.Selection.PickObjects(ObjectType.Element, new DWGLineSelectionFilter(), "Select lines from the DWG :").Select(reference =>
                 {
                     Element element = uidoc.Document.GetElement(reference);
-                    if (element is CurveElement curveElement)
-                    {
-                        return curveElement.GeometryCurve as Line;
-                    }
-                    return null;
-                }).Where(line => line != null).ToList();
+                    return element;
+                })
+                .Cast<ModelLine>()
+                .Where(line => line != null).ToList();
             }
             catch (Exception)
             {
-                throw;
             }
 
             return curves;
